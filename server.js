c1b531affd83fefc2538c0e3f68f7b53a7ac28df
Ishan Mitra <ishanbubby@gmail.com>
2016-06-28 17:20:17 -0700

    Add score and edit response based on score

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var request =  require('request');
 
+var score;
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -27,18 +29,24 @@ app.post('/sendFeatures', function(req, res) {
     json: true,
     body: {
       'app_id': "03523f3a331b11e694f706f4115cbc5d",
-      'feature_data': data.num_pregnant+','+data.glucose+','+data.pressure+','+data.riceps+','+data.serum+','+data.BMI+','+data.pedigree+','+data.age+','+data.diabetic,
-      'debug_mode': '2'
+      'feature_data': data.num_pregnant+','+data.glucose+','+data.pressure+','+data.riceps+','+data.serum+','+data.BMI+','+data.pedigree+','+data.age+',0',
+      'debug_mode': '1'
     }
   }, function(err, response, body) {
-    console.log(body.data.predict_score_response.pred_list[0]);
-    console.log("Error" + err);
+    console.log(body.data);
+    score = body.data.predict_score_response.pred_list[0] === 1 ? body.data.predict_score_response.prediction.class1 : body.data.predict_score_response.prediction.class0;
+  //  console.log("Error" + err);
+    console.log("Score: " + score);
     if(body.data.predict_score_response.pred_list[0] === 1) {
       res.sendFile(path.join(__dirname + '/diabetic.html'));
     } else {
       res.sendFile(path.join(__dirname + '/nondiabetic.html'));
     }
   });
+});
+
+app.get('/sendFeatures', function(req, res) {
+  res.sendStatus(score);
 });
 
 app.listen(8080);
