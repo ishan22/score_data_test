4cf0121a3f2c1a17575f38297fc633baea671478
Ishan Mitra <ishanbubby@gmail.com>
2016-08-11 10:33:29 -0700

    Change UI to work for breast cancer model

--- server.js
+++ server.js
@@ -28,19 +28,20 @@ app.post('/sendFeatures', function(req, res) {
     },
     json: true,
     body: {
-      'app_id': "03523f3a331b11e694f706f4115cbc5d",
-      'feature_data': data.num_pregnant+','+data.glucose+','+data.pressure+','+data.riceps+','+data.serum+','+data.BMI+','+data.pedigree+','+data.age+',0',
+      'app_id': "32de6aca577a11e6862106f4115cbc5d",
+      'feature_data': data.patient_id+','+data.clump+','+data.cell_size+','+data.cell_shape+','+data.adhesion+','+data.single_cell+','
+      +data.bare_nuclei+','+data.chromatin+','+data.nucleoli+','+data.mitoses+',0',
       'debug_mode': '1'
     }
   }, function(err, response, body) {
-    console.log(body.data);
+    console.log(body);
     score = body.data.predict_score_response.pred_list[0] === 1 ? body.data.predict_score_response.prediction.class1 : body.data.predict_score_response.prediction.class0;
   //  console.log("Error" + err);
     console.log("Score: " + score);
     if(body.data.predict_score_response.pred_list[0] === 1) {
-      res.sendFile(path.join(__dirname + '/diabetic.html'));
+      res.sendFile(path.join(__dirname + '/malignant.html'));
     } else {
-      res.sendFile(path.join(__dirname + '/nondiabetic.html'));
+      res.sendFile(path.join(__dirname + '/benign.html'));
     }
   });
 });
