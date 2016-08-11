014bdaa452c8afc55f7b4e2c48a8e9b7cb670fb5
Ishan Mitra <ishanbubby@gmail.com>
2016-08-11 16:25:40 -0700

    Finish homepage

--- server.js
+++ server.js
@@ -54,6 +54,34 @@ app.post('/cancerApp', function(req, res) {
   });
 });
 
+//post request from diabetic_form.html
+app.post('/diabeticApp', function(req, res) {
+  var data = req.body;
+  request({
+    url: 'http://ishan:ScoreData4321@console.scoredata.com/api/score/',
+    method: 'POST',
+    headers: {
+       'Content-Type': "application/json"
+    },
+    json: true,
+    body: {
+      'app_id': "03523f3a331b11e694f706f4115cbc5d",
+      'feature_data': data.num_pregnant+','+data.glucose+','+data.pressure+','+data.riceps+','+data.serum+','+data.BMI+','+data.pedigree+','+data.age+',0',
+      'debug_mode': '1'
+    }
+  }, function(err, response, body) {
+    console.log(body.data);
+    score = body.data.predict_score_response.pred_list[0] === 1 ? body.data.predict_score_response.prediction.class1 : body.data.predict_score_response.prediction.class0;
+  //  console.log("Error" + err);
+    console.log("Score: " + score);
+    if(body.data.predict_score_response.pred_list[0] === 1) {
+      res.sendFile(path.join(__dirname + '/diabetic.html'));
+    } else {
+      res.sendFile(path.join(__dirname + '/nondiabetic.html'));
+    }
+  });
+});
+
 app.get('/sendFeatures', function(req, res) {
   res.send({
     'score': score
